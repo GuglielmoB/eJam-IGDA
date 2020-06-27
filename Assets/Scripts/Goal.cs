95fb29a75e25c20148324eaedb127315c52e2d5f
GuglielmoB <GuglielmoB97@gmail.com>
2020-06-27 18:43:52 -0400

    Added checking for win condition, currently have to stay in goal for 3 seconds. Countdown resets as soon as the player leaves the goal's collider.

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour
+{
+    public bool isColliding;
+    public float countdown = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isColliding == true)
+        {
+            countdown -= Time.deltaTime;
+            if (countdown < 0)
+            {
+                countdown = 0;
+            }
+
+            
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Balancer")
+        {
+            print("Player Entered");
+            isColliding = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+
+        if (isColliding == true)
+        {
+            if (countdown <= 0)
+            {
+                SceneManager.LoadScene("EndScreen");
+            }
+
+        }
+       
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Balancer")
+        {
+            print("Player Exited");
+            isColliding = false;
+            countdown = 3.0f;
+        }
+    }
+}
